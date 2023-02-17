706ab42deab34e57275db6b7479564e1bf7a9d53
Gustavo <ga2157724@gmail.com>
2023-02-16 23:54:29 -0300

    adding ButtonText in header

--- src/pages/NewMovie/index.jsx
+++ src/pages/NewMovie/index.jsx
@@ -1,12 +1,25 @@
 import {Container} from "./style"
 import { Header } from "../../components/Header"
-
+import {ButtonText} from "../../components/ButtonText"
+import {FiArrowLeft} from "react-icons/fi"
 
 export function NewMovie() {
   return (
    <Container>
     <Header/>
-  
+      <FiArrowLeft/>
+      <ButtonText
+      title = "Voltar"
+      />
+    <main>
+      <Form>
+       <header>
+        <h1>Novo Filme</h1>
+       </header>
+       
+       
+      </Form>
+    </main>
 
    </Container>
   )
