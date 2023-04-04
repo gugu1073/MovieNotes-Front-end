15ba64296027b15ec5bec5f3b2d7bb0c23c8f543
Gustavo <ga2157724@gmail.com>
2023-04-03 21:33:10 -0300

    adding userNavegate

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Container, Form, Background } from "./style";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import {api} from "../../services/api";
 
@@ -13,6 +13,8 @@ export function SignUp() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const navigate = useNavigate();
+
     function handleSignUp() {
     if(!name || !email || !password) {
      return alert("Preencha todos os campos!")
@@ -20,14 +22,15 @@ export function SignUp() {
 
      api.post("/users", {name, email, password})
      .then(() => {
-      alert("Usuário cadastrado com sucesso")
+       alert("Usuário cadastrado com sucesso!");
+       navigate("/");
      })
 
      .catch(error => {
        if(error.response) {
         alert(error.response.data.message)
        }else {
-        alert("Não foi possivel cadastar")
+        alert("Não foi possível cadastar")
        }
      });
   }
